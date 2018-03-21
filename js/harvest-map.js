46875fd2a216fdeb8e350e4755677bf76db1c343
twburton <36517587+twburton@users.noreply.github.com>
2018-03-21 13:53:11 -0400

    Add files via upload

--- js/harvest-map.js
+++ js/harvest-map.js
@@ -21,6 +21,29 @@ svgTexas.style.display = 'none';
 
 settable=0;
 var timeout;
+
+
+//var svg = d3.select("#harvestmap");
+document.addEventListener("DOMContentLoaded", function() {
+
+    ["object", "iframe", "embed", "img"].forEach(function(elmtType) {
+      var e, threeElmts = document.querySelectorAll(elmtType);
+      e = threeElmts[0];
+      if (e.contentDocument) e.parentElement.replaceChild(e.contentDocument.documentElement.cloneNode(true), e);
+      e = threeElmts[1];
+      if (e.getSVGDocument) e.parentElement.replaceChild(e.getSVGDocument().documentElement.cloneNode(true), e);
+      e = threeElmts[2];
+      var xhr = new XMLHttpRequest();
+      xhr.open("GET", e.getAttribute(e.nodeName === "OBJECT" ? "data" : "src"));
+      xhr.send();
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) e.outerHTML = xhr.responseText;
+      };
+    });
+  
+});
+
+
 document.getElementById('svg1').addEventListener('mouseover', function(ev){
 
 	$("path, circle").mouseenter(function(e) {
